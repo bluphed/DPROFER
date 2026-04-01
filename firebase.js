03c6b746ae576e6e8957888a1e18511cabb67ae1
bluphed <otroyo699@gmail.com>
2026-03-31 19:17:25 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD...",
-    authDomain: "dproferinventario.firebaseapp.com",
-    projectId: "dproferinventario",
-    storageBucket: "dproferinventario.firebasestorage.app",
-    messagingSenderId: "123456789012",
-    appId: "1:123456789012:web:abcdef123456"
+    apiKey: "AIzaSyBJ0oZ_yuermDV-dn05msGkzM01Tr3feGo",
+    authDomain: "dprofer-7bea9.firebaseapp.com",
+    projectId: "dprofer-7bea9",
+    storageBucket: "dprofer-7bea9.firebasestorage.app",
+    messagingSenderId: "788368470526",
+    appId: "1:788368470526:web:e0b7f6e648f04f2ef150af"
 };
